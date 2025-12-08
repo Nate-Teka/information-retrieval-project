b5388af471353f26883b44129c2a68d20d7ec98d
Nathaniel Tekalgn <nathanieltekalgn@gmail.com>
2025-12-08 11:33:00 +0300

    This is an initialization push

---
+++ src/main/java/com/example/information_retrieval_backend/InformationRetrievalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.information_retrieval_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InformationRetrievalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InformationRetrievalBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/information_retrieval_backend/InformationRetrievalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.information_retrieval_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InformationRetrievalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
